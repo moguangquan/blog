39ee4a624ee91f7a98cc0cf2f262b1928efd7fd0
moguangquan <2759188793@qq.com>
2016-03-10 21:32:37 +0800

    样式和js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -6,9 +6,25 @@ $(function() {
     animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
   });
 });
-
+/**
+ * 分类展示
+ * 点击右侧的分类展示时
+ * 左侧的相关裂变展开或者收起
+ * @return {[type]} [description]
+ */
+function categoryDisplay() {
+    /*only show All*/
+    $('.post-list-body>div[post-cate!=All]').hide();
+    /*show category when click categories list*/
+    $('.categories-list-item').click(function() {
+        var cate = $(this).attr('cate'); //get category's name
+        $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
+        $('.post-list-body>div[post-cate=' + cate + ']').show(400);
+    });
+}
 // FitVids options
 $(function() {
+  categoryDisplay();
   $("article").fitVids();
 });
 
