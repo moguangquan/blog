6ca853a53341b9c23c9c5c6c3c5f78d47c40c166
moguangquan <2759188793@qq.com>
2016-03-12 14:37:34 +0800

    添加标签页面js和布局修改

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,21 +1,21 @@
-/*分类的js */
+/*标签的js */
 /**
  * 分类展示
- * 点击左侧的分类展示时
+ * 点击左侧的标签展示时
  * 右侧的相关裂变展开或者收起
  * @return {[type]} [description]
  */
-function categoryDisplay() {
-    /*only show All*/
-    $('.post-list-body>div[post-cate!=All]').hide();
-    /*show category when click categories list*/
+function tagDisplay() {
+    /*only show first tag*/
+    $('.post-list-body>div[post-cate!=first-tag]').hide();
+    /*show tag when click tag list*/
     $('.categories-list-item').click(function() {
-        var cate = $(this).attr('cate'); //get category's name
+        var cate = $(this).attr('cate'); //get tag's name
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
         $('.post-list-body>div[post-cate=' + cate + ']').show(400);
     });
 }
 // FitVids options
 $(function() {
-  categoryDisplay();
+  tagDisplay();
 });
