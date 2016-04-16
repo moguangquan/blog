8df9607198e34a2196c56761c0a48b5c43136f1b
moguangquan <2759188793@qq.com>
2016-04-16 18:13:40 +0800

    目录分类样式修改

--- assets/js/classfications.js
+++ assets/js/classfications.js
@@ -7,7 +7,7 @@
  */
 function categoryDisplay() {
     /*only show All*/
-    $('.post-list-body>div[post-cate!=All]').hide();
+    $('.post-list-body>div[post-cate!=first-cate]').hide();
     /*show category when click categories list*/
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
