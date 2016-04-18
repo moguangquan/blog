2b578b0a18c119feb795e671c5256d8ed2a78302
moguangquan <2759188793@qq.com>
2016-04-19 00:08:56 +0800

    测试。。。

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -38,22 +38,28 @@ function generatePagi() {
         if(cur_page-1>2){
             $cur_tag_posts.find("a.post-list-item").hide();//隐藏文章
             $cur_tag_posts.find("a.post-list-item").slice((cur_page-1)*2,cur_page*2).show();//显示文章
-            $cur_tag_posts.find("a[cur_page='"+(cur_page-2)+"']").closest("li").prevAll().hide();//隐藏页码
-            $cur_tag_posts.find("a[cur_page='"+(cur_page+2)+"']").closest("li").nextAll().show();//显示页码
-        }
-        //判断页码-总页数>=(可选的页数/2)
-        if(cur_page-1<2){
+            //隐藏所有页码
+            $cur_tag_posts.find("div.pagination").find("li").hide();
+            //显示五个页码slice(cur_page-2-1,cur_page+2+1)
+            $cur_tag_posts.find("div.pagination").find("li").slice(cur_page-2-1,cur_page+2).show();
+            //$cur_tag_posts.find("a[cur_page='"+(cur_page-2)+"']").closest("li").prevAll().hide();//隐藏页码
+            //$cur_tag_posts.find("a[cur_page='"+(cur_page+2)+"']").closest("li").nextAll().show();//显示页码
+        }else{//判断页码-总页数>=(可选的页数/2)
             $cur_tag_posts.find("a.post-list-item").hide();//隐藏文章
             $cur_tag_posts.find("a.post-list-item").slice((cur_page-1)*2,cur_page*2).show();//显示文章
-            $cur_tag_posts.find("a[cur_page='"+(cur_page+2)+"']").closest("li").nextAll().hide();//隐藏页码
-            $cur_tag_posts.find("a[cur_page='"+(cur_page-2)+"']").closest("li").prevAll().show();//显示页码
+           //隐藏所有页码
+            $cur_tag_posts.find("div.pagination").find("li").hide();
+           //显示五个页码slice(cur_page-2-1,cur_page+2+1)
+            $cur_tag_posts.find("div.pagination").find("li").slice(0,cur_page+2).show();
+           // $cur_tag_posts.find("a[cur_page='"+(cur_page+2)+"']").closest("li").nextAll().hide();//隐藏页码
+           // $cur_tag_posts.find("a[cur_page='"+(cur_page-2)+"']").closest("li").prevAll().show();//显示页码
         }
     });
     //前一页
     $("div.pagination strong.prev").on("click",function(){
+        var $cur_tag_posts=$(this).closest("div.pagination");//得到当前的tag
         var cur_page=$cur_tag_posts.find("a.current-page").attr("cur_page");//获取当前的页码
         tag_reset();//复原操作
-        var $cur_tag_posts=$(this).closest("div.pagination");//得到当前的tag
         if(cur_page){
             var prev_page=cur_page-1;
             //给前一个页码添加current-page样式
@@ -64,9 +70,9 @@ function generatePagi() {
     });
     //后一页
     $("div.pagination strong.next").on("click",function(){
+        var $cur_tag_posts=$(this).closest("div.pagination");//得到当前的tag
         var cur_page=$cur_tag_posts.find("a.current-page").attr("cur_page");//获取当前的页码
         tag_reset();//复原操作
-        var $cur_tag_posts=$(this).closest("div.pagination");//得到当前的tag
         if(cur_page){
             var next_page=cur_page+1;
             $cur_tag_posts.find("a[cur_page='"+(next_page)+"']").attrClass("current-page");//给前一个页码添加current-page样式
@@ -83,6 +89,8 @@ function tag_reset(){
     $("div.pagination a").removeClass("current-page");
 //2.隐藏所有文章
     $("a.post-list-item").hide();
+//3.显示所有的页码
+    $("div.pagination li").show();
 }
 // FitVids options
 $(function() {
