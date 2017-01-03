7760c02670d6468106bae89129f24a09d0114c7f
moguangquan <2759188793@qq.com>
2017-01-03 22:15:13 +0800

    升级版archives

--- assets/js/archives.js
+++ assets/js/archives.js
@@ -1,12 +1,15 @@
 /*归档分类的js */
 /**
- * 分类展示
- * 点击左侧的分类展示时
- * 右侧的相关裂变展开或者收起
- * @return {[type]} [description]
- */
-function archiveDisplay() {
+*初始化操作
+*/
+function initArchive(){
+  //导航栏中只显示今年的archives,并把非今年的archive隐藏
+    var currDate=new Date();
+    var currYear=currDate.getFullYear();
+    $('a.categories-list-item').hide();//隐藏所有archives
+    $('a.categories-list-item').find("a[cate^="+currYear+"]").show();//显示当年的archives
    /*only show first cate*/
+
     var $first_archive_posts=$('.post-list-body').find('div:first');
     $first_archive_posts.nextAll().hide();//隐藏除第一个文章块下的文章
     //初始化第一个标签的隐藏文章
@@ -17,21 +20,58 @@ function archiveDisplay() {
     if($first_archive_paginator){
         $first_archive_paginator.find("li:first").addClass("current-page");//给页码一添加current-page样式
     }
+}
+/**
+*根据传入的cate找到需要隐藏和显示的文章列表
+*/
+function opArchives(var cate){
+    archive_reset();
+    var $cur_archive_posts=$(document.getElementsByName(cate)[0]);
+    $('.post-list-body>div').hide(250);
+    $cur_archive_posts.show(400);
+    //隐藏该标签下的部分文章
+    $cur_archive_posts.find("a.post-list-item").hide();//隐藏文章
+    $cur_archive_posts.find("a.post-list-item").slice(0,5).show();//显示文章
+     //第一个标签下的页码
+    var $cur_archive_paginator=$cur_archive_posts.find("div.pagination");
+    if($cur_archive_paginator){
+      $cur_archive_paginator.find("a:first").addClass("current-page");//给页码一添加current-page样式
+    }
+}
+/**
+ * 分类展示
+ * 点击左侧的分类展示时
+ * 右侧的相关裂变展开或者收起
+ * @return {[type]} [description]
+ */
+function archiveDisplay() {
+    initArchive();
+    //点击archive导航栏条目
     $('.archives').click(function() {
-      archive_reset();
-      var cate = $(this).attr('cate'); //get archive name
-      var $cur_archive_posts=$(document.getElementsByName(cate)[0]);
-      $('.post-list-body>div').hide(250);
-      $cur_archive_posts.show(400);
-      //隐藏该标签下的部分文章
-      $cur_archive_posts.find("a.post-list-item").hide();//隐藏文章
-      $cur_archive_posts.find("a.post-list-item").slice(0,5).show();//显示文章
-       //第一个标签下的页码
-      var $cur_archive_paginator=$cur_archive_posts.find("div.pagination");
-      if($cur_archive_paginator){
-        $cur_archive_paginator.find("a:first").addClass("current-page");//给页码一添加current-page样式
-      }
+        var cate = $(this).attr('cate'); //get archive name
+        opArchives(cate);
     });
+    //点击前一年的链接跳转
+    $('#prevYear').click(function() {
+        var prevYear=$('#currYear').attr("title")-1;//获取当页年份-1
+        opPrevOrNextYear(prevYear);
+    }
+    //点击下一年的链接跳转
+    $('#nextYear').click(function() {
+        var nextYear=$('#currYear').attr("title")+1;//获取当页年份+1
+        opPrevOrNextYear(nextYear);
+    }
+}
+/**
+*点击前一年或后一年的链接跳转操作
+*/
+function opPrevOrNextYear(var opYear){
+    $('a.categories-list-item').hide();//隐藏所有archives
+    var $cates=$('a.categories-list-item').find("a[cate^="+opYear+"]");
+    $cates.show();//获取导航栏目，显示当页年的archives
+    var cate=$cates.first().attr("cate");//获取第一条
+    opArchives(cate);
+    $('#currYear').attr("title",opYear);//设置当页年份
 }
 /**
 *生成分页
